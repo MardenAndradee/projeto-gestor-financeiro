dcf6902adaa760057da7390d1e00feb73fd046e1
marden <marden.andrade1@gmail.com>
2025-03-17 22:29:43 -0300

    Adicionando tela de cadastro

---
+++ src/frontend/app/cadastro/page.tsx
@@ -0,0 +1,9 @@
+import CadastroCard from "./CadastroCard";
+
+export default function CadastroPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
+      <CadastroCard />
+    </div>
+  );
+}
